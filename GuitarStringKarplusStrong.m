98f4bca730cc5ff4629c52e6fd99716e2e1d7728
Aditya Jeganath <109350395+TropicThunder2000@users.noreply.github.com>
2023-12-16 04:36:11 +0530

    Update

--- GuitarStringKarplusStrong.m
+++ GuitarStringKarplusStrong.m
@@ -11,11 +11,9 @@ x_n = 0; % this is a random value for part 1
 
 x_nm1 = 0; % this is a random value for part 1
 
-% 1. difference_equation(y_n) with x_n(current input), x_nm1(previous input), a(brightness), d(decay factor)
+% difference_equation(y_n) with x_n(current input), x_nm1(previous input), a(brightness), d(decay factor)
 
 y_n = d * ( a * x_n + (1-a) * x_nm1 );
-
-% 3. your chord or melody
 
 Note1 = k_strong(440, fs, d);
 
@@ -52,8 +50,6 @@ pause(0.5);
 % 2. karplus strong algorithm
 
 function y = k_strong(f, fs, d)
-
-% when we are at the 1st sample of the buffer, the "previous" sample of a circular buffer is actually the last sample
 
  T = 3; % duration of a note
 
